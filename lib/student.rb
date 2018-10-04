fcadc6813783463b38028a863f6957c884901aaa
Justine Bricknell <justinegayton@gmail.com>
2018-10-04 14:39:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,6 @@
 class Student # Remember, you can access your database connection anywhere in this class with DB[:conn]
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(name, grade, id=nil)
     @name = name
@@ -31,6 +32,6 @@ class Student # Remember, you can access your database connection anywhere in th
     student = Students.new(name, grade)
     student.save
     student
-  end 
+  end
 
 end
